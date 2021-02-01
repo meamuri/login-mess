9a5835ab5fc134cd695ce0acbd7b872f2d904e63
Roman Dronov <meammf@yandex.ru>
2021-02-01 19:33:55 +0300

    Improve form

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -4,6 +4,19 @@
   (:require [reagent.core :as reagent]
             [reagent.dom :as rdom]))
 
+(defn form 
+  []
+  (fn []
+    [:form#login-form.pt-2.pb-2
+     [:div.form-row.mb-3
+      [:label {:for "login-input"} "Username"]
+      [:input#login-input {:type "text"}]]
+     [:div.form-row.mb-3
+      [:label {:for "password-input"} "Password"]
+      [:input#password-input {:type "password"}]]
+     [:div.form-footer
+      [:button#sign-in-button "Sign in"]]]))
+
 (defn ^:private app 
   "Application entry point component"
   []
@@ -15,12 +28,7 @@
      [:div#app-name 
       [:h2 "An Application"]]
      [:div#form-container {:style {:width 480 :height 150}}
-      [:form#login-form
-       [:label {:for "login-input"} "Username"]
-       [:input#login-input {:type "text"}]
-       [:br]
-       [:label {:for "password-input"} "Password"]
-       [:input#password-input {:type "password"}]]]]
+      [form]]]
     [:div#footer
      [:p {:dangerouslySetInnerHTML {:__html "&copy; 2021, created by Roman Dronov"}}]]]])
 
